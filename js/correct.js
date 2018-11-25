d8dcf667ba8dea3cd2854436720229b53eb5c603
Romain PASDELOUP <romainpasdeloup@outlook.com>
2018-11-25 22:25:41 +0100

    Little update
    Fixed regex, still need improvements

--- js/correct.js
+++ js/correct.js
@@ -1,11 +1,16 @@
 var words = ['asshole','con','merde','salope','salop','salopes','salops','saloppe','bougnoul','bounioul','bougnoulle','bougnoule','pd','gouine','gouines','goudou','gouinasse','gouinasses','youpin','négro','bamboula','emburqinée','emburquiné','emburquinées','emburquinés','bicot','bico','bicots','crouille','crouilles','raton','youpin','youpins','tafiole','taffiole','tafioles','taffioles','tantouse','tantouze','tantouses','tarlouses','tarlouse','tarlouze','pute','putasse','pupute','puputes','putes','pétasse','petasse','petasses','pétasses','tasspé','tasspe','connasse','conasse','connasses','conasses','pouffiasse','pouffiasses','pouffiace','pouff','pouffiasse','pouf','pouffe','hollandouille','enculé','enculés','encules','godmiché','godmich','godmicher','sarkonard','sarkonnard','nabot','naboleon','naboléon','bouffonne','boufone','bouffone','bouffonnes','bouffon','boufon','boufons','bouffons','couille','couilles','connar','connard','conard','connards','conards','catin','katin','catins','katins','suceuse','suçeuse','suceuses','conne','connes','garce','garces','chiasse','chieur','chieurs','chieuses','chieuse','ducon','duconne','saloperie','bâtard','bâtards','bâtarde','bâtardes','batard','batarde','batards','batardes','couillon','sarkonnasse','sarkonasse','branleur','branleurs','lopette','lopettes','lopetes','lopete','pédé','pédés','pédérastes','pédéraste','radasse','salopard','salopart','salopar','mongolien','mongolienne','gogolène','bouffon','bouffons','trouduc','fdp','enfoiré','enfoirés','merdasse','merdasses','merdouille','merdouilles','raclure','raclures','negresse','négresse','salaud','salauds','tdq','battard','battards','tarlouze','tarlouzes','connace','musulmerde','juiffon','connars'];
-var expr = '';
+var expr = '(\\b';
 words.forEach(function(element) {
   if(element != words[words.length-1]) {
-		expr += element +' |';
+		expr += element +'|\\b';
 	} else {
 		expr += element +' ';
 	}
 });
+expr += ')'
+console.log(expr);
 var rExp = new RegExp(expr, 'gi');
-document.body.innerHTML.replace(rExp, "baka");
+window.onload = replaceBaka;
+function replaceBaka() {
+  document.body.innerHTML = document.body.innerHTML.replace(rExp, "baka");
+}
